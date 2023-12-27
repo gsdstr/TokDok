3c61f8f116b5354b59f582d38aaf26fcef14af5d
gsdstr <gsdstr@gmail.com>
2023-12-27 13:29:51 +0300

    init api

---
+++ README.md
@@ -0,0 +1,5 @@
+# Server
+
+`docker build . -t tokdok_api`
+
+`docker run -p 8080:8080 tokdok_api`
---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos { get; set; } = null!;
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseNpgsql("User ID=tokdok;Password=super_strong_password;Server=localhost;Port=5433;Database=tokdok;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
